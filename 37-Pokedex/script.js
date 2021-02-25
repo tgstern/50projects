7d7be19a69033c46571a6e963dff64f3713def1b
Taylor Gawinski Stern <57155433+tgstern@users.noreply.github.com>
2021-02-25 10:12:20 -0500

    Add files via upload

---
+++ 37-Pokedex/script.js
@@ -0,0 +1,55 @@
+const poke_container = document.getElementById('poke-container')
+const pokemon_count = 151
+const colors = {
+    fire: '#FDDFDF',
+    grass: '#DEFDE0',
+	electric: '#FCF7DE',
+	water: '#DEF3FD',
+	ground: '#f4e7da',
+	rock: '#d5d5d4',
+	fairy: '#fceaff',
+	poison: '#98d7a5',
+	bug: '#f8d5a3',
+	dragon: '#97b3e6',
+	psychic: '#eaeda1',
+	flying: '#F5F5F5',
+	fighting: '#E6E0D4',
+	normal: '#F5F5F5'
+}
+
+const main_types = Object.keys(colors)
+
+const fetchPokemon = async () => {
+    for(let i = 1; i <= pokemon_count; i++) {
+        await getPokemonById(i)
+    }
+}
+
+const getPokemonById = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    createPokemonCard(data)
+}
+
+const createPokemonCard = (pokemon) => {
+    const pokemonEl = document.createElement('div')
+    pokemonEl.classList.add('pokemon')
+    const poke_types = pokemon.types.map(type => type.type.name)
+    const type = main_types.find(type => poke_types.indexOf((type)) > -1)
+    pokemonEl.style.backgroundColor = colors[type]
+    const pokemonInnerHTML = `
+        <div class="img-container">
+            <img src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="${pokemon.name}" />
+        </div>
+        <div class="info">
+            <span class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
+            <h3 class="name">${pokemon.name}</h3>
+            <small class="type">Type: <span>${type}</span></small>
+        </div>
+    `
+    pokemonEl.innerHTML = pokemonInnerHTML
+    poke_container.append(pokemonEl)
+}
+
+fetchPokemon()
