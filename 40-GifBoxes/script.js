ecd9ea0ed6d691c2846b002decece7bc1f643cc0
Taylor Gawinski Stern <57155433+tgstern@users.noreply.github.com>
2021-03-09 13:04:15 -0500

    Add files via upload

---
+++ 40-GifBoxes/script.js
@@ -0,0 +1,17 @@
+const boxes = document.getElementById('boxes')
+const btn = document.getElementById('btn')
+
+function createBoxes() {
+    for(let i = 0; i < 4; i++) {
+        for(let j = 0; j < 4; j++) {
+            const box = document.createElement('div')
+            box.classList.add('box')
+            box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+            boxes.appendChild(box)
+        }
+    }
+}
+
+createBoxes()
+
+btn.addEventListener('click', () => boxes.classList.toggle('big'))
