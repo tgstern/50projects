f32b03da2139370e52ab7765fbe793b1316b0746
Taylor Gawinski Stern <57155433+tgstern@users.noreply.github.com>
2021-03-12 12:34:09 -0500

    Add files via upload

---
+++ 43-FeedbackUI/script.js
@@ -0,0 +1,29 @@
+const ratings = document.querySelectorAll('.rating')
+const send = document.querySelector('#send')
+const panel = document.querySelector('#panel')
+const ratingsContainer = document.querySelector('.ratings-container')
+let selectedRating = 'Satisfied'
+
+ratingsContainer.addEventListener('click', (e) => {
+    if(e.target.parentNode.classList.contains('rating')) {
+        removeActive()
+        e.target.parentNode.classList.add('active')
+        selectedRating = e.target.nextElementSibling.innerHTML
+    }
+})
+
+send.addEventListener('click', (e) => {
+    panel.innerHTML = `
+        <i class="fas fa-heart"></i>
+        <strong>Thank you!</strong>
+        <br>
+        <strong>Feedback: ${selectedRating}</strong>
+        <p>We'll use your feedback to improve our customer experience</p>
+    `
+})
+
+function removeActive() {
+    for (let i = 0; i < ratings.length; i++) {
+        ratings[i].classList.remove('active')
+    }
+}
