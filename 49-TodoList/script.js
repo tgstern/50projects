99223a0a994c9ef5bc749a92d947b5cd4ac76811
Taylor Gawinski Stern <57155433+tgstern@users.noreply.github.com>
2021-03-19 12:17:54 -0400

    Add files via upload

---
+++ 49-TodoList/script.js
@@ -0,0 +1,52 @@
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const todosUL = document.getElementById('todos')
+
+const todos = JSON.parse(localStorage.getItem('todos'))
+
+if (todos) {
+    todos.forEach(todo => {addTodo(todo)})
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addTodo()
+})
+
+function addTodo(todo) {
+    let todoText = input.value
+    if (todo) {
+        todoText = todo.text
+    }
+    if (todoText) {
+        const todoEl = document.createElement('li')
+        if (todo && todo.complete) {
+            todoEl.classList.add('complete')
+        }
+        todoEl.innerText = todoText
+        todoEl.addEventListener('click', () => {
+            todoEl.classList.toggle('complete')
+            updateLS()
+        })
+        todoEl.addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+            todoEl.remove()
+            updateLS()
+        })
+        todosUL.appendChild(todoEl)
+        input.value = ''
+        updateLS()
+    }
+}
+
+function updateLS() {
+    todosEl = document.querySelectorAll('li')
+    const todos = []
+    todosEl.forEach(todoEl => {
+        todos.push({
+            text: todoEl.innerText,
+            complete: todoEl.classList.contains('complete')
+        })
+    })
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
